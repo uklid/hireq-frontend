85b784e153248fbc4cb97b570f96db2351b6ffe3
พิชยะ แสนยะ <pitchaya.senya@gmail.com>
2018-05-27 21:14:27 +0700

    Add some word for Psersonal report

--- src/containers/Candidates/ReportPersonal.js
+++ src/containers/Candidates/ReportPersonal.js
@@ -188,20 +188,19 @@ export default () => (
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="PERFORMANCE AND PROJECT MANAGEMENT"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="The Performance and Project Management (PPM) scale score reflects Uklid
+          Yeesarapat's ability  to handle work, lead projects, and manage the performance of others and he."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="LEADERSHIP AND ORGANIZATIONAL MANAGEMENT"
+          title="LEADERSHIP AND ORGANIZATIONAL MANAGEMEN"
           score="70"
           firstColor="green"
           secondColor="green"
@@ -209,72 +208,70 @@ export default () => (
           secondTitle="WATCH"
           description="The Leadership and Organizational Management (LOM) scale score measures how
           good Uklid Yeesarapat is at developing goals and strategies, delegating tasks, and
-          motivating others to achieve them"
+          motivating others to achieve them."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="COMMUNICATION, PERSUASION AND NEGOTIATION"
           score="40"
           firstColor="green"
           secondColor="lightgreen"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="The Communication. Persuasion and Negotiation (CPN) scale score provides a gauge
+          of Uklid Yeesarapat's ability to communicate, convince and deal effectively with
+        supervisors co-workers, and subordlinates, as well as people outside the company"
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="PEOPLE AND INTERPERSONAL SKILLS"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="The people and interpersonal skills (PIS) scale score indicates one's ability to interact
+          and coperate with other, both individualy in group, in professional situaltions"
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="POLITICAL AND CULTURAL SKILLS"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="The Political and Cultural Skills (PCS) scale score measures Uklid Yeesarapat's ability
+          to adjust to different people and situations, as well as #posPronoun openness to
+          change and to considerable variety in the workplace."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="PRODUCTIVITY AND EFFECTIVENESS AT WORK"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="The Productivity and Effectiveness at Work (PEW) scale score measures Uklid
+          Yeesarapat's positive and productive behavors, as well as how one avoids behaviors
+        and attitudes that negatively impact job performance."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="SELF-MANAGEMENT MOTIVATION AND CONTROL"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="TThe Self-management, Motivation and Control (SMC) scale score is an indicator of a person's emotional intelligence (EQ) and related qualities, such as self-control,
+          persistence, initiative, and stress tolerance."
         />
       </Grid>
       <Grid item xs={3}>
@@ -282,12 +279,12 @@ export default () => (
       </Grid>
       <Grid item xs={10}>
         <GreyWrapper>
-          <h4>Critical Softskills</h4>
+          <h4>Work Preferences</h4>
           <p style={{ fontSize: 12 }}>
-            Cognitive ability is brain-base skills that candidate need to carry out work task.
-              It difines the mechanism of how people learn solve problems, and make dicisions.
-              It relates to many essential functions of the brain such as memory, perception, attension
-              , and visual and spatial processing.
+            Work preferences measure the candidate 's interest profile.
+             Our wark preferences profiler masures the candidate's job interest in six
+             dimensions and determines whetner it matches the interest proflie required
+             to enjoy and succeed in the job
           </p>
         </GreyWrapper>
       </Grid>
@@ -305,80 +302,79 @@ export default () => (
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="PHYSICAL, PRACTICAL AND MECHANICAL"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who prefer Physical, Practical and Mechanica typically like work activities that
+          include practical, hands-on problems and solutions. They will enioy working outside, in jobs that do not involve a lot of paperwork or much interaction with others."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="INTELLECTUAL, ANALYTICAL AND METHODOLOGICAL"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who are Intellectual. Analytical and Methodological prefer occupations that
+          frequently involve working with ideas and require an extensive amount of thinking.They will enjoy processing information, facts and figuring out problems mentally."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="CREATIVE, INNOVATIVE AND IMAGINATIVE"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who are Creative. Innovative and lmaginative prefer 
+          jobs that give them room to let fly of their original self, and avoid orderad or
+          repetitive acdles at all cost. They will enjoy work that can be done without lo
+          following a set a clear set of rules."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="INTERPERSONAL, COOPERATIVE AND HUMANISTIC"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who are Interpersonal, Cooperative and Humanistic typically like to interact,
+          educate and do things that help people, like teaching, nursing, or protecting. They will
+          enjoy jobs that involve helping or providing seivice to others."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="PERSUASIVE, ADVENTUROUS AND MOTIVATIONAL"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who are Persuasive. Adventurous and Motivational like to lead and persuade
+          people and to sell things or ideas.They will enjoy positions that frequently involve starting up 
+          and carrying out projects, making important decisions, and leading others."
         />
       </Grid>
       <Grid item xs={3}>
         <CardWithStatus
-          title="GENERAL INTELLIGENCE AND LEARNING ABILITY "
+          title="CONSCIENTIOUS, EFFICIENT AND ORDERLY"
           score="70"
           firstColor="green"
           secondColor="green"
           firstTitle="HIGH"
           secondTitle="WATCH"
-          description="The General Intelligence and Learning Ability (GIL) scale score reflects Uklid
-          Yeesarapat's global intelligence and capacity to reason, solve problems, and learn new
-        skills and abilities in new situations."
+          description="People who are Conscientious, Efficient and Orderly like to wok with numbers,
+          records, or machines in a set, orderly, and clearly laid out way, following strict
+          procedures and routines. They wil enioy working with data and details more than with ideas."
         />
       </Grid>
       <Grid container spacing={8} style={{ margin: 0 }}>
